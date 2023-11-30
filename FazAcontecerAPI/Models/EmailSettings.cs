7282073a5f557823ac3452fe62a246c1c9a943aa
Sabine Freiman <sabinefreiman@gmail.com>
2023-11-30 15:07:40 -0300

    Envio do Convite

---
+++ FazAcontecerAPI/Models/EmailSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FazAcontecerAPI.Models
+{
+    public class EmailSettings
+    {
+        public string PrimaryDomain { get; set; }
+
+        public int PrimaryPort { get; set; }
+
+        public string UsernameEmail { get; set; }
+
+        public string UsernamePassword { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string NotificationTo { get; set; }
+    }
+
+}
